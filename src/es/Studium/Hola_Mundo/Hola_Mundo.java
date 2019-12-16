859e2d1a382a832bb9d5f292c48319003f1f1349
AlvCarballo <alv.carballo@outlook.es>
2019-12-16 13:27:52 +0100

    Creacion del fichero java hola mundo

---
+++ src/es/Studium/Hola_Mundo/Hola_Mundo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package es.Studium.Hola_Mundo;
+
+/**
+ * @author alvaro
+ *
+ */
+public class Hola_Mundo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola Mundo");
+	}
+
+}
